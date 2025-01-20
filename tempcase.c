bddf7bc97fee951f06b82f6a575f344d322d7690
卡尔 <129243132+aikedaerC@users.noreply.github.com>
2025-01-21 02:18:08 +0800

    Create tempcase.c

---
+++ tempcase.c
@@ -0,0 +1,10 @@
+int /* PRIVATE */
+png_set_text_2(png_structp png_ptr, png_infop info_ptr, png_textp text_ptr, int num_text)
+{
+   int i;
+   png_debug1(1, "in %s storage function", (png_ptr->chunk_name[0] == '\0' ?
+                                           "text" : (png_const_charp)png_ptr->chunk_name));
+   if (png_ptr == NULL || info_ptr == NULL || num_text == 0)
+       return(0);
+    ...
+}
